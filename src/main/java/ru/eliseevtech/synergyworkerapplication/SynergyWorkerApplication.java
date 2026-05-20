d6e4ceed27e2100772d8e711f0afa99b70f7f640
Eliseev Aleksandr <eliseevtech@icloud.com>
2026-05-20 08:25:35 +0300

    init

---
+++ src/main/java/ru/eliseevtech/synergyworkerapplication/SynergyWorkerApplication.java
@@ -0,0 +1,13 @@
+package ru.eliseevtech.synergyworkerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynergyWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynergyWorkerApplication.class, args);
+    }
+
+}
